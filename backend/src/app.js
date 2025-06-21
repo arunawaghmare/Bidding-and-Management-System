e244510a54d743bc26b93dcbfa0ff0990316b63d
arunawaghmare <53028975+arunawaghmare@users.noreply.github.com>
2025-06-21 16:29:38 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,12 @@ import reviewRoutes from "./routes/reviewRoutes.js";
 
 import dotenv from "dotenv";
 dotenv.config();
+app.use(
+  cors({
+    origin: "https://bidding-and-management-system-m45wflrbx.vercel.app", // your Vercel frontend domain
+    credentials: true,
+  })
+);
 const app = express();
 app.use(cors());
 app.use(express.json());
